175c0000a0d4819aed4c4ce10fce1eb688a55420
Martin Johansen <martin.johansen.98@outlook.com>
2023-01-17 21:07:59 +0100

    random string added

--- Git.js
+++ Git.js
@@ -1,26 +1,47 @@
-const fs = require('fs');
+/**
+ * Create a function that wil return a random 64 bit string to ./data.json
+ * use simple-git to push new changes to "hotfix" branch in origin repo.
+ * 
+ * create a pull request to merge "hotfix" branch to "main" branch
+*/
+
 const simpleGit = require('simple-git');
-const git = simpleGit.default();
+const git = simpleGit();
+const fs = require('fs');
+const path = require('path');
 
-const GitGraph = async () => {
-    try {
-        
-        // use fs to write a random 64 character string to a file.json 
-        // this will be used as the commit message
-        const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        fs.writeFileSync('./data.json', JSON.stringify({ randomString }));
-        
-        // push change to hotfix branch
-        await git.add('./*');
-        await git.commit(randomString);
-        await git.push('origin', 'hotfix');
-
-        console.log(`${randomString} pushed to hotfix branch`)
-        
-
-    } catch (e) {
-        console.log(e);
-    }
+const randomString = () => {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
-GitGraph();
+const data = {
+    "randomString": randomString()
+}
+
+fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(data, null, 2));
+
+git.add('./*')
+    .commit('random string added')
+    .push('origin', 'hotfix', () => {
+        console.log('pushed to hotfix branch');
+    });
+
+git.pull('origin', 'main', () => {
+    console.log('pulled from main branch');
+});
+
+git.checkout('main', () => {
+    console.log('checked out to main branch');
+});
+
+git.mergeFromTo('hotfix', 'main', () => {
+    console.log('merged hotfix branch to main branch');
+});
+
+git.push('origin', 'main', () => {
+    console.log('pushed to main branch');
+});
+
+git.checkout('hotfix', () => {
+    console.log('checked out to hotfix branch');
+});
