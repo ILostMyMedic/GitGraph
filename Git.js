1a664d46a6375f5771f7dd18e1519985c5c24c05
Martin Johansen <martin.johansen.98@outlook.com>
2023-01-17 21:00:28 +0100

    1mk4j3xfsj181f9289t3mh

--- Git.js
+++ Git.js
@@ -2,21 +2,9 @@ const fs = require('fs');
 const file = require('./data.json');
 const simpleGit = require('simple-git');
 const git = simpleGit.default();
-const { Octokit } = require("@octokit/core");
-
 
 const GitGraph = async () => {
     try {
-        // onfiguration for octokit
-        const octokit = new Octokit({
-            auth: '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',
-        });
-        const {
-            data: { login },
-        } = await octokit.request('GET /user');
-        console.log("Hello, %s", login);
-        
-        
         
         // use fs to write a random 64 character string to a file.json 
         // this will be used as the commit message
@@ -28,21 +16,7 @@ const GitGraph = async () => {
         await git.commit(randomString);
         await git.push('origin', 'hotfix');
 
-        // create a pull request from hotfix to main
-        const { data } = await octokit.pulls.create({
-            owner: 'ILostMyMedic',
-            repo: 'GitGraph',
-            title: randomString,
-            head: 'hotfix',
-            base: 'main',
-        });
-
-        // merge pull request
-        await octokit.pulls.merge({
-            owner: 'ILostMyMedic',
-            repo: 'GitGraph',
-            pull_number: data.number,
-        });
+        
 
     } catch (e) {
         console.log(e);
